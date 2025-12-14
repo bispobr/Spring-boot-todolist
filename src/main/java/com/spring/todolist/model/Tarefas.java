b5c4f63430fc69b3836a877f6e931bc729492bc1
bispobr <156684382+bispobr@users.noreply.github.com>
2025-12-14 15:54:56 -0300

    refact: remoção de contrutor

--- src/main/java/com/spring/todolist/model/Tarefas.java
+++ src/main/java/com/spring/todolist/model/Tarefas.java
@@ -1,6 +1,5 @@
 package com.spring.todolist.model;
 
-import com.spring.todolist.dto.TarefaRequisicaoDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -25,9 +24,4 @@ public class Tarefas {
     private String descricao;
     private boolean completo;
 
-    public Tarefas(TarefaRequisicaoDto tarefa) {
-        this.titulo = tarefa.titulo();
-        this.descricao = tarefa.descricao();
-        this.completo = tarefa.completo();
-    }
 }
